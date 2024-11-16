f129a52522dbf2516987f501822d3bdf4cc16b65
reinanbr <perseu912@gmail.com>
2024-11-16 09:37:18 -0300

    upgrade

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { Express, Request, Response, NextFunction } from "express";
+import nunjucks from "nunjucks";
+
+const PORT = 3000;
+const server: Express = express();
+
+// Configuração de middlewares
+server.use('/static',express.static(__dirname + "/static"));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// Configuração do Nunjucks
+server.set("view engine", "njk");
+nunjucks.configure("src/views", {
+    express: server,
+    autoescape: false,
+    noCache: true,
+});
+
+
+// Rotas
+server.get('/', (req: Request, res: Response) => {
+  res.render('index', { title: 'Página Inicial', message: 'Bem-vindo ao meu site!' });
+});
+
+server.get('/about', (req: Request, res: Response) => {
+  res.render('about', { title: 'Sobre Nós', message: 'Informações sobre nossa empresa.' });
+});
+
+server.get('/contact', (req: Request, res: Response) => {
+  res.render('contact', { title: 'Contato', message: 'Entre em contato conosco.' });
+});
+
+// Iniciar o servidor
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
