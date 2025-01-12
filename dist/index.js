cfbc5efc2d398db332415363db7dc164aca388f1
reinanbr <perseu912@gmail.com>
2025-01-12 07:04:18 -0300

    upload

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,54 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const nunjucks_1 = __importDefault(require("nunjucks"));
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
 const PORT = 3000;
 const server = (0, express_1.default)();
 // Configuração de middlewares
@@ -18,19 +62,53 @@ nunjucks_1.default.configure("./src/views", {
     autoescape: false,
     noCache: true,
 });
+// Função para carregar o JSON
+const loadSimulations = (filePath) => {
+    try {
+        const data = fs.readFileSync(filePath, 'utf-8');
+        return JSON.parse(data);
+    }
+    catch (err) {
+        console.error('Erro ao carregar o arquivo JSON:', err);
+        return null;
+    }
+};
+const readFileAsync = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+};
+/*-------------------------------------------------------------*/
 // Rotas
-server.get('/', (req, res) => {
-    res.render('index', { title: 'Simulações Da Filosofia Natural', message: 'Bem-vindo ao meu site!' });
-});
+server.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const article_db = path.join(__dirname, "db/article_db/doc_db.json");
+    const dataArticles = yield loadSimulations(article_db);
+    const simsDb = path.join(__dirname, "/db/sims_db/simsData.json");
+    const dataSimulations = yield loadSimulations(simsDb);
+    res.render('index', { articles: dataArticles, sims: dataSimulations, title: 'Simulações Da Filosofia Natural', message: 'Bem-vindo ao meu site!' });
+}));
 server.get('/about', (req, res) => {
     res.render('about', { title: 'Sobre Nós', message: 'Informações sobre nossa empresa.' });
 });
 server.get('/contact', (req, res) => {
     res.render('contact', { title: 'Contato', message: 'Entre em contato conosco.' });
 });
-server.get("/sims/quanta", (req, res) => {
-    res.render("sims/modernPhysics/quantumMechanics/tunelling/index");
-});
+server.get("/sims/:sim_key", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const sim_key = req.params.sim_key;
+    const simsDb = path.join(__dirname, "/db/sims_db/simsData.json");
+    const dataSimulations = yield loadSimulations(simsDb);
+    const sims = dataSimulations.find(sim => sim.key === sim_key);
+    const contentDataFile = path.join(__dirname, sims.content);
+    const contentData = yield readFileAsync(contentDataFile);
+    res.render("layouts/base_sim", { thumb: sims.thumb, title: sims.title, description: sims.description, content: contentData });
+}));
 server.get('/sim/:area/:code_sim', (req, res) => {
     const { area, code_sim } = req.params;
     res.json({
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import nunjucks from "nunjucks";
 
+import * as fs from 'fs';
+import * as path from 'path';
+
+
 const PORT = 3000;
 const server: Express = express();
 
@@ -18,9 +22,50 @@ nunjucks.configure("./src/views", {
 });
 
 
+
+/*---------------------------------------------*/
+interface Simulation {
+  key:string;
+  title: string;
+  description: string;
+  content:string;
+  thumb:string;
+}
+
+// Função para carregar o JSON
+const loadSimulations = (filePath: string): Simulation[] | null => {
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data) as Simulation[];
+  } catch (err) {
+    console.error('Erro ao carregar o arquivo JSON:', err);
+    return null;
+  }
+};
+
+
+const readFileAsync = (filePath: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+/*-------------------------------------------------------------*/
+
 // Rotas
-server.get('/', (req: Request, res: Response) => {
-  res.render('index', { title: 'Simulações Da Filosofia Natural', message: 'Bem-vindo ao meu site!' });
+server.get('/',async (req: Request, res: Response) => {
+	const article_db:string = path.join(__dirname,"db/article_db/doc_db.json");
+	const dataArticles = await loadSimulations(article_db);
+
+	const simsDb: string = path.join(__dirname, "/db/sims_db/simsData.json");
+	const dataSimulations = await loadSimulations(simsDb); 
+ 	
+	res.render('index', {articles:dataArticles,sims:dataSimulations, title: 'Simulações Da Filosofia Natural', message: 'Bem-vindo ao meu site!' });
 });
 
 server.get('/about', (req: Request, res: Response) => {
@@ -31,9 +76,17 @@ server.get('/contact', (req: Request, res: Response) => {
   res.render('contact', { title: 'Contato', message: 'Entre em contato conosco.' });
 });
 
-server.get("/sims/quanta",(req:Request,res:Response)=>{
-	
-	res.render("sims/modernPhysics/quantumMechanics/tunelling/index");
+
+server.get("/sims/:sim_key", async (req:Request,res:Response)=>{
+	const sim_key = req.params.sim_key;
+
+	const simsDb: string = path.join(__dirname, "/db/sims_db/simsData.json");
+	const dataSimulations = await loadSimulations(simsDb); 
+	const sims = dataSimulations.find(sim => sim.key === sim_key);
+
+	const contentDataFile = path.join(__dirname,sims.content);	
+	const contentData = await readFileAsync(contentDataFile);
+	res.render("layouts/base_sim",{thumb:sims.thumb,title:sims.title,description:sims.description,content:contentData});
 });
 
 
